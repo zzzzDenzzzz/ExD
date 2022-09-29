bc5f095b9cbf86aa6de2df1ed8749e1ff962fbb8
12den24@gmail.com <12den24@gmail.com>
2022-09-29 22:12:20 +0300

    Добавьте файлы проекта.

---
+++ ExD/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExD
+{
+    internal class MyDictionary
+    {
+        public string TypeDictionary { get; }
+        SortedList<string, List<string>> dictionary;
+
+        public MyDictionary(string typeDictionary)
+        {
+            TypeDictionary = typeDictionary;
+            dictionary = new SortedList<string, List<string>>() { };
+        }
+
+        public static MyDictionary CreateDictionary(string typeDictionary)
+        {
+            return new MyDictionary(typeDictionary);
+        }
+
+        public void Add(string word, string translate)
+        {
+            if (dictionary.ContainsKey(word))
+            {
+                if (!dictionary[word].Contains(translate))
+                {
+                    dictionary[word].Add(translate);
+                }
+                else
+                {
+                    throw new ArgumentException("error: class MyDictionary->Add->if->if()");
+                }
+            }
+            else
+            {
+                dictionary.Add(word, new List<string> { translate });
+            }
+        }
+
+        public void Print()
+        {
+            foreach (var itemKey in dictionary)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(itemKey.Key);
+                foreach (var itemValue in itemKey.Value)
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(itemValue);
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ExD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new MyDictionary("e - r");
+            var d1 = MyDictionary.CreateDictionary("r - e");
+            try
+            {
+                d.Add("word2", "перевод0");
+                d.Add("word1", "перевод0");
+                d.Add("word0", "перевод0");
+                d.Add("word0", "перевод1");
+                d.Add("word2", "перевод1");
+                d.Print();
+
+                d1.Add("слово2", "translete0");
+                d1.Add("слово1", "translete0");
+                d1.Add("слово0", "translete0");
+                d1.Add("слово0", "translete1");
+                d1.Add("слово2", "translete1");
+                d1.Print();
+            }
+            catch (ArgumentException argEx)
+            {
+                Console.WriteLine(argEx.Message);
+            }
+        }
+    }
+}
